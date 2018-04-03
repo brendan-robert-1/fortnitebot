309aa69d38ccc935023139a25897fd25eb786af0
= <=>
2018-04-03 21:36:27 +0000

    updated help options

--- stats.py
+++ stats.py
@@ -6,7 +6,6 @@ import pprint
 import myconfig
 from discord.ext import commands
 
-print(myconfig.appId)
 allowableScopes = ['s3','lifetime']
 allowableGameModes = ['solo','duo','squad','solos','duos','squads','total']
 bot = commands.Bot(command_prefix='!',description='Fortnite Stats')
@@ -31,6 +30,9 @@ async def help(ctx):
     embed = discord.Embed(title="Fortnite stats bot", description = "Gets fortnite stats for a given user")
     embed.add_field(name="!kills", value="Gets total kills for a user based on game mode and scope.\nTakes exactly 3 arguments: \n\nusername[epic username]\nscope [lifetime, s3]\ngame_mode [solo, duo, squad]\n\nExamples: \n\t!kills PikachuDownB s3 solo\n\t!kills Faemn lifetime duo", inline=False)
     embed.add_field(name="!kdr", value ="Gets K/D ratio for a user based on game mode and scope.\nTakes exactly 3 arguments: \n\nusername[epic username]\nscope [lifetime, s3]\ngame_mode[solo,duo,squad]\n\nExamples:\n\t!kdr PikachuDownB s3 solo\n\t!kdr Ninja lifetime squad", inline =False)
+    embed.add_field(name="!winrate", value ="Gets win rate for a user based on game mode and scope.\nTakes exactly 3 arguments: \n\nusername[epic username]\nscope [lifetime, s3]\ngame_mode[solo,duo,squad]\n\nExamples:\n\t!winrate PikachuDownB s3 solo\n\t!winrate Ninja lifetime squad", inline =False)
+    embed.add_field(name="!trn", value ="Gets TRN rating for a user based on game mode and scope.\nTakes exactly 3 arguments: \n\nusername[epic username]\nscope [lifetime, s3]\ngame_mode[solo,duo,squad]\n\nExamples:\n\t!kdr PikachuDownB s3 solo\n\t!trn Ninja lifetime squad", inline =False)
+    embed.add_field(name="!source", value="Source code for this bot", inline=True)
     await bot.say(embed = embed)
 
 @bot.command(pass_context=True)
@@ -59,10 +61,9 @@ async def kills(ctx, user, scope, game_mode='total'):
         else:
             kill_stats = get_stat_dict(user, scope, game_mode,'kills')
             amount = kill_stats['value']
-            rank = kill_stats['rank']
             percentile = kill_stats['percentile'];
             scopeStr = get_scope_str(scope)
-            msg = user + " has " + str(amount) + " total kills in " + game_mode + scopeStr + scope + ". This puts " + user + " in the top " +str(percentile) + " percentile, at rank " + str(rank) + "."
+            msg = user + " has " + str(amount) + " total kills in " + game_mode + scopeStr + scope + ". This puts " + user + " in the top " +str(percentile) + " percentile."
             await bot.say(msg)
 
 @bot.command(pass_context=True)
@@ -75,9 +76,8 @@ async def kdr(ctx, user, scope, game_mode):
         kdr_stats = get_stat_dict(user, scope, game_mode, 'kd')
         value = kdr_stats['value']
         percentile = str(kdr_stats['percentile'])
-        rank = str(kdr_stats['rank'])
         scopeStr = get_scope_str(scope)
-        msg = user + " has a K/D ratio of " + value + " for " + game_mode+ scopeStr + scope + ". This puts " + user + " in the top " + percentile + " percentile, at rank " + rank + "." 
+        msg = user + " has a K/D ratio of " + value + " for " + game_mode+ scopeStr + scope + ". This puts " + user + " in the top " + percentile + " percentile." 
         await bot.say(msg)
 
 @bot.command(pass_context=True)
@@ -90,9 +90,8 @@ async def trn(ctx, user, scope, game_mode):
         trn_stats = get_stat_dict(user, scope, game_mode, 'trnRating')
         value = trn_stats['displayValue']
         percentile = str(trn_stats['percentile'])
-        rank = str(trn_stats['rank'])
         scopeStr = get_scope_str(scope)
-        msg = user + " has a TRN Rating of " + value + " for " + game_mode + scopeStr + scope + ". This puts " + user + " in the top " + percentile + " percentile, at rank " + rank + "."
+        msg = user + " has a TRN Rating of " + value + " for " + game_mode + scopeStr + scope + ". This puts " + user + " in the top " + percentile + " percentile."
         await bot.say(msg)
 
 @bot.command(pass_context=True)
@@ -105,9 +104,8 @@ async def winrate(ctx, user, scope, game_mode):
         win_stats = get_stat_dict(user, scope, game_mode, 'winRatio')
         value = win_stats['displayValue']
         percentile = str(win_stats['percentile'])
-        rank = str(win_stats['rank'])
         scopeStr = get_scope_str(scope)
-        msg = user + " has a win ratio of " + value + "% for " + game_mode + scopeStr + scope + ". This puts " + user + " in the top " + percentile + " percentile, at rank " + rank + "."
+        msg = user + " has a win ratio of " + value + "% for " + game_mode + scopeStr + scope + ". This puts " + user + " in the top " + percentile + " percentile."
         await bot.say(msg)
 
 @bot.command(pass_context=True)
@@ -116,6 +114,9 @@ async def yikes(ctx):
   embed.add_field(name="yikes", value="Y  I  K  E  S", inline=False)
   await bot.say(embed = embed)
 
+@bot.command(pass_context=True)
+async def source(ctx):
+    await bot.say('Source code for this bot can be be found at https://github.com/brendan-robert-1/fortnitebot')
 
 fnTrackerUrl="https://api.fortnitetracker.com/v1/profile/pc/"
 auth={'TRN-Api-Key':myconfig.trn}
@@ -153,7 +154,6 @@ def get_stat_dict(user, scope, game_mode, stat):
 
 def main():
     try:
-        pp = pprint.PrettyPrinter(depth=6)
         print("Starting fortnite stats bot...")
         bot.run(myconfig.appId)
     finally:
